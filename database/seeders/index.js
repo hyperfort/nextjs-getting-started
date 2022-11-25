678d2990edf6b7fe4076460be577df2293224c1a
Miguel Elizondo <miguelelz@outlook.com>
2022-11-24 19:48:21 -0800

    feat: add seed to fill the users table

---
+++ database/seeders/index.js
@@ -0,0 +1,5 @@
+const UsersTableSeeder = require("./users-table-seeder");
+
+(async function () {
+  await UsersTableSeeder.run();
+})();
---
+++ database/seeders/users-table-seeder.js
@@ -0,0 +1,23 @@
+const { Role } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
+const prisma = require("../../lib/prisma");
+
+class UsersTableSeeder {
+  static async run() {
+    await prisma.user.upsert({
+      where: {
+        email: "john.doe@nextjs.org",
+      },
+      update: {},
+      create: {
+        name: "John Doe",
+        email: "john.doe@nextjs.org",
+        password: bcrypt.hashSync("&elements.rope.562&", bcrypt.genSaltSync()),
+        role: Role.ADMIN,
+      },
+    });
+  }
+}
+
+module.exports = UsersTableSeeder;

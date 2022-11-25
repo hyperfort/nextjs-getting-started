4890c7a9f5023e53da3c80611db25801a4b6cf48
Miguel Elizondo <miguelelz@outlook.com>
2022-11-24 19:18:38 -0800

    feat: add Login page

---
+++ components/forms/login-form.js
@@ -0,0 +1,84 @@
+import { useForm } from "react-hook-form";
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function _onSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(_onSubmit)}>
+      <section className="mb-6">
+        <label
+          className={["form-label", errors.email && "text-red-500"].join(" ")}
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          className={[
+            "form-input",
+            errors.email
+              ? "border-red-500 mb-2"
+              : "border-gray-300 focus:border-blue-600",
+          ].join(" ")}
+          id="email"
+          placeholder="e.g. someone@example.com"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Email not valid",
+            },
+          })}
+        />
+        <p className="italic text-red-500 text-xs">{errors.email?.message}</p>
+      </section>
+
+      <section className="mb-6">
+        <label
+          className={["form-label", errors.password && "text-red-500"].join(
+            " "
+          )}
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          className={[
+            "form-input",
+            errors.password
+              ? "border-red-500 mb-2"
+              : "border-gray-300 focus:border-blue-600",
+          ].join(" ")}
+          id="password"
+          placeholder="* * * * * * * * * * * *"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+          })}
+        />
+        <p className="italic text-red-500 text-xs">
+          {errors.password?.message}
+        </p>
+      </section>
+
+      <section className="text-center lg:text-left">
+        <button className="form-button">Login</button>
+      </section>
+    </form>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import LoginForm from "../components/forms/login-form";
+
+export default function Login() {
+  return (
+    <main className="flex flex-wrap g-6 h-screen items-center justify-center px-6 text-gray-800 lg:justify-between xl:justify-center">
+      <section className="basis-auto grow-0 flex justify-center mb-12 shrink-1 md:mb-0 md:shrink-0 md:w-9/12 lg:w-6/12 xl:w-6/12">
+        <Image
+          src="/logo.webp"
+          width={800}
+          height={534}
+          priority={true}
+          className="w-4/5"
+          alt="Sample image"
+        />
+      </section>
+      <section className="mb-12 md:mb-0 xl:ml-20 md:w-8/12 lg:w-5/12 xl:w-5/12">
+        <LoginForm />
+      </section>
+    </main>
+  );
+}
